d1f16f11f44fc3336e52bff94766e127d37c9375
Alex Charland <ac3charland@gmail.com>
2019-01-22 18:50:56 -0600

    Displaying text to page. Still no image from data.

--- public/js/test.js
+++ public/js/test.js
@@ -9,8 +9,33 @@ $(document).ready(function() {
         console.log(res)
     })
     .then(function(response) {
-        console.log("Here's the .then response from /api/books:")
-        console.log(response)
-        res.json(response)
+        console.log("Here's the .then response from /api/books:");
+        console.log(response[0]);
+        var res = $("<ul>");
+
+        var author = $("<li>");
+        author.text(response[0].author);
+
+        var title = $("<li>");
+        title.text(response[0].title);
+
+        var description = $("<li>");
+        description.text(response[0].description);
+
+        var imageEnclosure = $("<li>");
+        var image = $("<img>");
+        var srcString = "data:" + response[0].image.contentType + ";" + response[0].image.data.data;
+        image.attr("src", srcString);
+        imageEnclosure.append(image);
+
+        res.append(author);
+        res.append(title);
+        res.append(description);
+        res.append(imageEnclosure);
+
+
+        $("#results").append(res);
+        res.json(response[0]);
+
     })
 })
