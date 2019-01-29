70ce7ff44262c67c4c693cbefeb73dfb0449b40b
Alex Charland <ac3charland@gmail.com>
2019-01-29 12:50:49 -0600

    Login/logout functionality working.

--- public/js/login.js
+++ public/js/login.js
@@ -9,6 +9,13 @@ $(document).ready(function() {
         let email = $("#registerEmail").val();
         let password1 = $("#registerPassword1").val();
         let password2 = $("#registerPassword2").val();
+
+        if (password1 !== password2) {
+            $("#confirmPasswordHelp").text("Passwords do not match.");
+            return
+        } else {
+            $("#confirmPasswordHelp").text("");
+        }
         let newUser = {
             user: {
                 email: email,
@@ -16,17 +23,20 @@ $(document).ready(function() {
                 passwordCheck: password2
             }
         }
-        $.post("/api/users/", newUser).then(function (response) {
+        $.post("/api/users/", newUser).then(function (response, err) {
             console.log(response);
+            if (err) {
+                console.log(err);
+            }
             $("#response").text(JSON.stringify(response));
             console.log("Saving token: " + response.user.token);
-            // Clear the previous cookie by setting it it equal to nothing and its expiration date to a past time
-            document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-            document.cookie = "token=" + response.user.token + "; Secure; HttpOnly"
-            console.log("Printing cookies:")
-            console.log(document.cookie);
-        })
-    })
+            // // Clear the previous cookie by setting it it equal to nothing and its expiration date to a past time
+            // document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+            // document.cookie = "token=" + response.user.token + "; Secure; HttpOnly";
+            localStorage.setItem("ffj", response.user.token);
+            location = "/";
+        });
+    });
     $("#login-submit").submit(function(event) {
         event.preventDefault()
 
@@ -43,10 +53,12 @@ $(document).ready(function() {
             $("#response").text(JSON.stringify(response));
             console.log("Saving token: " + response.user.token);
             // Clear the previous cookie by setting it it equal to nothing and its expiration date to a past time
-            document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
-            document.cookie = "token=" + response.user.token + "; Secure; HttpOnly"
-            console.log("Printing cookies:")
-            console.log(document.cookie);
+            // document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+            // document.cookie = "token=" + response.user.token + "; Secure; HttpOnly"
+            // console.log("Printing cookies:")
+            // console.log(document.cookie);
+            localStorage.setItem("ffj", response.user.token);
+            location = "/";
         })
 
     })
---
+++ public/js/navbar.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    console.log("Navbar script is running!");
+    const token = localStorage.getItem("ffj");
+
+    if (token) {
+        console.log("Got token!")
+        console.log(token)
+        $.ajax({
+            url: "/api/users/current",
+            type: 'GET',
+            // Fetch the stored token from localStorage and set in the header
+            headers: { "Authorization": "Token " + token }
+        }).then(function(response) {
+            console.log(response);
+            $("#login-text-item").attr("hidden", false);
+            $("#logout-item").attr("hidden", false);
+            $("#login-item").attr("hidden", true);
+            $("#login-text").text("Logged in as " + response.user.email);
+        }); 
+    }
+
+    $("#logout-link").click(function(event) {
+        localStorage.removeItem("ffj");
+    })
+})
