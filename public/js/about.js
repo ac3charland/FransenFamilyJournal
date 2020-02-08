af39b5ef941bb3209c962c6201393a6a2b9c74ab
Alex Charland <ac3charland@gmail.com>
2020-02-08 08:29:18 -0600

    Add login error messaging. Clarify copy. Remove account creation JSON display on page. Make title of entry optional. Dynamically hide navigation links when selected.

---
+++ public/js/about.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $("#about-item").attr("hidden", true)
+})
--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,35 +1,41 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    const pathName = window.location.pathname;
     const token = localStorage.getItem("ffj");
 
+    $("#about-item").attr("hidden", false)
     if (token) {
         $.ajax({
             url: "/api/users/current",
             type: 'GET',
             // Fetch the stored token from localStorage and set in the header
             headers: { "Authorization": "Token " + token }
-        }).then(function(response) {
+        }).then(function (response) {
             $("#login-text-item").attr("hidden", false);
             $("#logout-item").attr("hidden", false);
-            $("#compose-item").attr("hidden", false);
+            if (pathName === "/compose/") {
+                $("#compose-item").attr("hidden", true);
+            } else {
+                $("#compose-item").attr("hidden", false);
+            }
             $("#login-item").attr("hidden", true);
             $("#login-text").text("Logged in as " + response.user.email);
-        }); 
+        });
     }
 
-    $("#logout-link").click(function(event) {
-        localStorage.removeItem("ffj");
-    })
-
-    $('#help-link').click(function () {
-        $("#welcome-modal").modal();
-    })
-
-    $("#compose-link").click(function(event) {
+    $("#compose-link").click(function (event) {
         $.ajax({
             url: "/compose/",
             type: 'GET',
             // Fetch the stored token from localStorage and set in the header
             headers: { "Authorization": "Token " + token }
-        }); 
+        });
+    })
+
+    $("#logout-link").click(function (event) {
+        localStorage.removeItem("ffj");
+    })
+
+    $('#help-link').click(function () {
+        $("#welcome-modal").modal();
     })
 })
